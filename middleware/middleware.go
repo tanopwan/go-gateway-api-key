cd06820eab857d6a0acaba7cbfd8ca71ffea6f64
Tanopwan Tangprakit <tanopwan.tos@ascendcorp.com>
2018-03-23 15:28:07 +0700

    Test setup and teardown

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -19,7 +19,7 @@ var (
 // Service ...
 type Service interface {
 	GenerateAPIKey(appID string) (string, error)
-	ValidateAPIKey(appID string, key string) error
+	ValidateAPIKey(appID string, key string) (bool, error)
 }
 
 type middleware struct {
@@ -64,28 +64,40 @@ func (m *middleware) GenerateAPIKey(appID string) (string, error) {
 	return key, nil
 }
 
-func (m *middleware) ValidateAPIKey(appID string, key string) error {
+func (m *middleware) ValidateAPIKey(appID string, key string) (bool, error) {
+	if len(appID) < 16 || len(key) < 32 {
+		return false, fmt.Errorf("parameters are invalid")
+	}
+
+	rd := m.redisPool.Get()
+	defer rd.Close()
+
+	cacheAppID, _ := redis.String(rd.Do("GET", key))
+	if cacheAppID == appID {
+		fmt.Println("Validated Pass found cache for appID", appID)
+		return true, nil
+	}
 
 	appKey := appKey{}
 
 	command := fmt.Sprintf("SELECT * FROM %s WHERE APP_ID = $1 AND KEY = $2", TableName)
 	err := m.db.QueryRow(command, appID, key).Scan(&appKey.ID, &appKey.AppID, &appKey.Key, &appKey.Created)
 	if err == sql.ErrNoRows {
-		return fmt.Errorf("key is invalid")
+		return false, fmt.Errorf("key is invalid")
 	}
 
 	if pqErr, ok := err.(*pq.Error); ok {
-		return fmt.Errorf("failed to query api key with reason: %s : %s", pqErr.Code, pqErr.Message)
+		return false, fmt.Errorf("failed to query api key with reason: %s : %s", pqErr.Code, pqErr.Message)
 	}
 
 	if err != nil {
-		return fmt.Errorf("failed to query api key with error: %s", err.Error())
+		return false, fmt.Errorf("failed to query api key with error: %s", err.Error())
 	}
 
 	if appID != appKey.AppID || key != appKey.Key {
-		return fmt.Errorf("failed to query api key")
+		return false, fmt.Errorf("failed to query api key")
 	}
 
 	fmt.Println("Validated Pass ", appKey.ID)
-	return nil
+	return false, nil
 }
--- test/middleware_test.go
+++ test/middleware_test.go
@@ -2,12 +2,15 @@ package test
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/tanopwan/go-gateway-api-key/middleware"
 )
 
-func setup() *sql.DB {
+func setup() (*sql.DB, *redis.Pool) {
 	db, err := sql.Open("postgres", "postgres://app:password@localhost/tanopwan?sslmode=disable")
 	if err != nil {
 		panic(err)
@@ -17,43 +20,94 @@ func setup() *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	return db
+
+	// init redis pool
+	redisPool := &redis.Pool{
+		MaxIdle:     2,
+		IdleTimeout: 60 * time.Minute,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", ":6379")
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) > time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+
+	return db, redisPool
 }
 
-func shutdown() {
-
+func shutdown(db *sql.DB, appID string, key string) {
+	command := fmt.Sprintf("DELETE FROM %s WHERE APP_ID = $1 AND KEY = $2", middleware.TableName)
+	_, err := db.Exec(command, appID, key)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func TestCreateAPIKey(t *testing.T) {
-	db := setup()
+	db, _ := setup()
 	defer db.Close()
 
 	m := middleware.NewService(db, nil)
 
-	key, err := m.GenerateAPIKey("1234")
+	appID := "1234567812345678"
+	key, err := m.GenerateAPIKey(appID)
 	if err != nil {
 		t.Log(err.Error())
 		panic(err)
 	}
 	t.Log(key)
+	shutdown(db, appID, key)
 }
 
-func TestValidateAPIKey(t *testing.T) {
-	db := setup()
+func TestValidateAPIKeyFromDB(t *testing.T) {
+	db, redisPool := setup()
 	defer db.Close()
 
-	m := middleware.NewService(db, nil)
+	m := middleware.NewService(db, redisPool)
 
-	key, err := m.GenerateAPIKey("1234")
+	appID := "1234567812345678"
+	key, err := m.GenerateAPIKey(appID)
 	if err != nil {
 		t.Log(err.Error())
 		panic(err)
 	}
 	t.Log(key)
 
-	err = m.ValidateAPIKey("1234", key)
+	_, err = m.ValidateAPIKey(appID, key)
 	if err != nil {
 		t.Log(err.Error())
 		panic(err)
 	}
+	shutdown(db, appID, key)
+}
+
+func TestValidateAPIKeyFromRedis(t *testing.T) {
+	db, redisPool := setup()
+	defer db.Close()
+
+	m := middleware.NewService(db, redisPool)
+
+	rd := redisPool.Get()
+	defer rd.Close()
+
+	appID := "1234567812345678"
+	key := "12345678123456781234567812345678"
+	_, err := rd.Do("SETEX", key, int64(time.Hour/time.Second), appID)
+	if err != nil {
+		panic(err)
+	}
+
+	isCache, err := m.ValidateAPIKey(appID, key)
+	if err != nil {
+		t.Log(err.Error())
+		panic(err)
+	}
+
+	t.Log(isCache)
+	shutdown(db, appID, key)
 }
